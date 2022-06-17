5b23cf0854d184fc3811470f7afeb247c91a0600
Jay Sierra <jsierra99@live.com>
2022-06-16 21:55:59 -0700

    twitter clone done

---
+++ components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    BellIcon, HashtagIcon, BookmarkIcon, CollectionIcon, DotsCircleHorizontalIcon, MailIcon, UserIcon, HomeIcon
+} from "@heroicons/react/outline";
+import SidebarRow from './SidebarRow';
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+const Sidebar = () => 
+{
+  const { data: session } = useSession();
+//2:42:02
+  return (
+    <div className='flex flex-col col-span-2 items-center px-4 md:items-start'>
+        <img className='h-10 w-10 m-3' src="https://links.papareact.com/drq" alt="" />
+        <SidebarRow Icon={HomeIcon} title="Home" />
+        <SidebarRow Icon={HashtagIcon} title="Explore" />
+        <SidebarRow Icon={BellIcon} title="Notifications" />
+        <SidebarRow Icon={MailIcon} title="Messages" />
+        <SidebarRow Icon={BookmarkIcon} title="Bookmark" />
+        <SidebarRow Icon={CollectionIcon} title="Lists" />
+        <SidebarRow onClick={session ? signOut : signIn} Icon={UserIcon} title={session ? "Sign Out" : "Sign In"} />
+        <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ components/Widgets.tsx
@@ -0,0 +1,24 @@
+import { SearchIcon } from '@heroicons/react/outline';
+import React from 'react';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
+const Widgets = () => 
+{
+  return (
+    <div className='mt-2 px-2 col-span-2 hidden lg:inline'>
+        {/* Search */}
+        <div className='flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2'>
+            <SearchIcon className='h-5 w-5 text-gray-400' />
+            <input type="text" placeholder='Search Twitter' className='bg-transparent outline-none flex-1' />
+        </div>
+
+        <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName="sonnysangha"
+            options={{height: 1000}}
+        />
+    </div>
+  )
+}
+
+export default Widgets
